a3cae9c16bac1f96bc6f9ea26aadc715a5898bf2
rapzz27 <arafi5823@gmail.com>
2026-07-14 20:28:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-// JavaScript akan kita isi nanti
+const tombol = document.getElementById("startBtn");
+
+tombol.addEventListener("click", () => {
+    alert("Hai Dedee ❤️\n\nSemoga website kecil ini bisa bikin Dedee senyum.\nDari Mas yang selalu sayang sama Dedee 🥰");
+});
